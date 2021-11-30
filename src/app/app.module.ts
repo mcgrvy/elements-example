9bbc41ace5b648955393f3072f261d42b36591e6
Chris McGreevy <chrismcgreevy@hotmail.co.uk>
2021-11-30 22:32:47 +0000

    Some fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,8 +12,7 @@ import { LQuotesComponent } from './l-quotes/l-quotes.component';
     HttpClientModule
   ],
   providers: [HttpClientModule],
-  bootstrap: [],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  bootstrap: []
 })
 export class AppModule {
   constructor(private injector : Injector) { }
